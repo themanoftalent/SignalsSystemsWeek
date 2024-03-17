0bcbcd78f4d4de7287363764e9b89b24c1ffabb3
HalilDayi <120185611+HalilDayi@users.noreply.github.com>
2024-03-17 23:04:11 +0300

    hmwrk_week3

---
+++ WorkWeek4/Homework_week3_Halil_Dayi_17_03_2024_BubbleSort.m
@@ -0,0 +1,11 @@
+function array = BubbleSort(array)
+ for i = 1:length(array)
+     for j = 1:length(array)-i
+         if (array(j) > array(j+1))
+            temp = array(j+1);
+            array(j+1) = array(j);
+            array(j) = temp;
+         end
+     end
+ end
+end
