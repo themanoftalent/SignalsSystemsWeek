9d653d821a9e4cdc05f38a15c8b43eea88cb4aad
hakanyvs <114612506+hakanyvs@users.noreply.github.com>
2024-03-18 18:31:14 +0300

    Homework

---
+++ WorkWeek4/homework_week3_hakan_yavas_18_03_2024/vectorAddition.m
@@ -0,0 +1,11 @@
+vec1 = input('enter first vector');
+vec2 = input('enter second vector');
+
+if length(vec1) == length(vec2)
+    vecsum = vec1 + vec2;
+    disp('vector sum =');
+    disp(vecsum);
+else
+    disp('vectors are not same lenght')
+
+end
