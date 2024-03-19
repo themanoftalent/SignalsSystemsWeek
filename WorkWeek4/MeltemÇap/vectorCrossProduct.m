db067932ee36bd5e616f92a463310f79692559ec
meltemcap <123328871+meltemcap@users.noreply.github.com>
2024-03-19 22:16:21 +0300

    Delete WorkWeek4/MeltemÇap directory

--- WorkWeek4/MeltemÇap/vectorCrossProduct.m
+++
@@ -1,9 +0,0 @@
-vec1 = input('Enter the elements of the first vector:');
-vec2 = input('Enter the elements of the second vector:');
-
-if length(vec1) == 3 && length(vec2) == 3
-    result = cross(vec1, vec2);
-    disp(result);
-else
-    disp('Vectors are not 3 dimensional');
-end
--- WorkWeek4/MeltemÇap/vectorNormCalculation.m
+++
@@ -1,5 +0,0 @@
-vector = input('Enter a vector: ');
-
-normValue = sqrt(sum(vector.^2));
-
-disp(['Euclidean norm of the vector: ', num2str(normValue)]);
