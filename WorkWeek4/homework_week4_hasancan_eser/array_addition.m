d06e04628ee8820bd6a127d4cc8a7c1b206003d9
eserhasancan <154316228+eserhasancan@users.noreply.github.com>
2024-03-23 14:29:59 +0300

    Update array_addition.m

---
+++ WorkWeek4/homework_week4_hasancan_eser/array_addition.m
@@ -0,0 +1,9 @@
+vector_A = [1 , 2 , 3];
+vector_B = [4 , 5 , 6];
+
+if length(vector_A) == length(vector_B)
+    sum_vector = vector_A + vectorB;
+    disp(sum_vector);
+else
+    disp('Arrays must have same length');
+end    
