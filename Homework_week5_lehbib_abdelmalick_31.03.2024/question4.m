bd1229040bfbf6a98726f1c80aa8c80c2141de47
lehbobss <119317492+lehbobss@users.noreply.github.com>
2024-03-31 19:03:55 +0300

    Update question4.m

--- Homework_week5_lehbib_abdelmalick_31.03.2024/question4.m
+++ Homework_week5_lehbib_abdelmalick_31.03.2024/question4.m
@@ -1,1 +1,15 @@
+n = -5:15;
+y=zeros(size(n));
 
+for i = 1:length(n)
+    if n(i) ==3
+        y(i) = 2;
+    else if (n(i)==0 || n(i)==5 || n(i)==10)
+        y(i) = 1;
+    else
+        y(i)=0;
+    end
+    end
+end
+
+stem(n,y,"filled")
