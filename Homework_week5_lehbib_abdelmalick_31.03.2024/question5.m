0204a0518b7462bc883615d7ecffb800113071fd
lehbobss <119317492+lehbobss@users.noreply.github.com>
2024-03-31 19:04:21 +0300

    Update question5.m

--- Homework_week5_lehbib_abdelmalick_31.03.2024/question5.m
+++ Homework_week5_lehbib_abdelmalick_31.03.2024/question5.m
@@ -1,1 +1,11 @@
+f = @(t) 2 * cos(2 * pi * t / 0.2);
+t = linspace(0, 1, 1000); % 1000 discrete nokta
+y = f(t);
 
+plot(t, y, 'o',MarkerFaceColor='b',MarkerSize=1);
+
+xlabel('t');
+ylabel('y');
+title('f(t) discrete points');
+
+grid on;
