e7a6d4724b4861814782a7d7264a7d55e9022569
Mekan Çelebi <91728006+MekanCelebi@users.noreply.github.com>
2024-03-31 15:47:48 +0300

    Update question5.m

--- WorkWeek5/Homework_Week5_Mekan_Celebi_31.03.2024/question5.m
+++ WorkWeek5/Homework_Week5_Mekan_Celebi_31.03.2024/question5.m
@@ -1,1 +1,20 @@
+% Define the function
+f = @(t) 2 * cos(2 * pi * t / 0.2);
+
+% Define time vector with 1000 discrete points
+t = linspace(0, 1, 1000);
+
+% Evaluate the function at each time point
+y = f(t);
+
+% Plot discrete points
+plot(t, y, 'o', 'MarkerFaceColor', 'b', 'MarkerSize', 1);
+
+% Add labels and title
+xlabel('t');
+ylabel('f(t)');
+title('f(t) with Discrete Points');
+
+% Enable grid
+grid on;
 
