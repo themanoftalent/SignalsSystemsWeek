1841f8242feb6777356930235ddf05c219f3d383
Mehmet Akif Cifci <17935050+themanoftalent@users.noreply.github.com>
2024-04-03 08:54:24 +0300

    Delete WorkWeek4 directory

--- WorkWeek4/homework_week3-Tunahan-Sözener-18.03.2024/Vector Sorting.m
+++
@@ -1,6 +0,0 @@
-% File: sortVector.m
-
-function sorted_vector = sortVector(vector)
-    % Sort the vector in ascending order
-    sorted_vector = sort(vector);
-end
--- WorkWeek4/homework_week3_hakan_yavas_18_03_2024/vectorNormCalculate.m
+++
@@ -1,5 +0,0 @@
-vec = input('enter a vector');
-
-normValue = sqrt(sum(vec.^2));
-
-disp(['Euclidean norm a vector.', num2str(normValue)]);
