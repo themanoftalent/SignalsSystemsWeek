37fdb4e33c236ed7edaeeb128aa8fc878f48cba4
mertgoksu <mertgoksumg@gmail.com>
2024-04-14 23:25:39 +0300

    homework_week6_MertGoksu_14_04_2024
    Visualizing Step Function, Homework of Week 6, Mert Goksu.

---
+++ Works_assign/WorkWeek  (6)/Homework_week6_MertGoksu_14_04_2024/visualizing_step_function_MertGoksu_14_04_2024.m
@@ -0,0 +1,14 @@
+% Step Function
+t = -30:0.1:30;
+
+step_function = zeros(size(t));
+step_function(t >= 0) = 1;
+
+figure('Name','Visualizing Step Function','NumberTitle','off','Color','white');
+plot(t, step_function,'k', 'LineWidth', 2);
+title('Step Function');
+xlabel('Time(s)');
+ylabel('Amplitude');
+ylim([-1, 1.5]);
+legend('Step Function',Location='northeast')
+grid on;
