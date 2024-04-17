dfb4aa47ffcb7be96bd984d78a78dec73552e2f4
mysCod3r <m.yasinsenocak@gmail.com>
2024-04-17 14:12:11 +0300

    added homework week 6

---
+++ Works_assign/WorkWeek  (6)/homework_week6_Muhammed_Yasin_Senocak_17-04-2024/step_func.m
@@ -0,0 +1,13 @@
+title('Step Signal');
+xlabel('Time(s)');
+ylabel('Amplitude');
+legend('Step Signal', 'Location', 'southeast')
+
+t = -30:0.1:30;
+
+step_signal = zeros(size(t));
+step_signal(t >= 0) = 1;
+
+plot(t, step_signal,'k', 'LineWidth', 2);
+ylim([-0.5, 1.5]);
+grid on;
