c705613c99bac448563b959c9c5f3d21c9c54453
mysCod3r <m.yasinsenocak@gmail.com>
2024-04-17 14:31:41 +0300

    added week1 simple arithmetic operations

---
+++ Works_assign/WorkWeek  (1)/yasin_senocak/simple_arithmetic_operations.m
@@ -0,0 +1,11 @@
+% Simple Arithmetic Operations
+a = 10;
+b = 3;
+sum_ab = a + b;
+diff_ab = a - b;
+prod_ab = a * b;
+quot_ab = a / b;
+disp(['Sum: ', num2str(sum_ab)]);
+disp(['Difference: ', num2str(diff_ab)]);
+disp(['Product: ', num2str(prod_ab)]);
+disp(['Quotient: ', num2str(quot_ab)]);
