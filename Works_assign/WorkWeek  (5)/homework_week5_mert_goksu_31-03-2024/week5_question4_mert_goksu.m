4f5cc7e9a8178acc02c0a5ff076426a9af5a5eb1
mertgoksu <mertgoksumg@gmail.com>
2024-04-17 16:31:14 +0300

    practice-on-w5-q4-stem-plot

--- Works_assign/WorkWeek  (5)/homework_week5_mert_goksu_31-03-2024/week5_question4_mert_goksu.m
+++ Works_assign/WorkWeek  (5)/homework_week5_mert_goksu_31-03-2024/week5_question4_mert_goksu.m
@@ -1,6 +1,9 @@
 % Question 4
-
 x = -5:15;
+
+%y = x.*0;
+%y = x.*0 = zeros(size(x);
+
 y = zeros(size(x));
 
 for i = 1:length(x)
@@ -14,10 +17,19 @@ for i = 1:length(x)
     end
 end
 
-stem(x,y,'filled',MarkerFaceColor ='r', LineStyle='-',LineWidth=3,MarkerSize=5)
-grid on;
+% r = red, k = black, b = blue
+
+figure(1),stem(x,y,'filled',MarkerFaceColor ='b', LineStyle='-',LineWidth=2,MarkerSize=7)
+
+figure(2),plot(x,y,MarkerFaceColor="k",LineStyle="-.", LineWidth=2,MarkerSize=5)
+
+grid off;
+
 xlabel('x');
 ylabel('Amplitude');
+
 xlim([min(x) max(x)]);
-ylim([0 2.5])
-legend('Unit Impulse Signal', 'Location' , 'northeast');
+ylim([0 2.5]);
+
+legend('Unit Impulse Signal', 'Location' , 'best');
+
